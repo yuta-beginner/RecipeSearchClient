485b4bdf03338de25a4a0ea391b9ed5b2d97a663
s_yuta <sy03141995139@outlook.com>
2023-12-10 21:36:03 +0900

    initial commit

---
+++ app/src/main/java/com/websarva/wings/android/recipesearchclient/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.websarva.wings.android.recipesearchclient
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.websarva.wings.android.recipesearchclient.ui.theme.RecipeSearchClientTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RecipeSearchClientTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RecipeSearchClientTheme {
+        Greeting("Android")
+    }
+}
